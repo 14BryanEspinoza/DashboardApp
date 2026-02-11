1dbb7ae4ff6e04492d17da99ec2ad05ae1da7253
14bry <14bryanespinoza@gmail.com>
2026-02-10 18:06:29 -0600

    Feat: Estrucutra de Footer completa, con diseño (Desktop, Tablet, Mobile) y algunas correciones a los estilos del navbar

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,18 @@
-console.log("RUN");
+// Constantes
+const date = document.getElementById('date')
+
+// Métodos
+const currentDate = new Date()
+
+// Manejo de Fecha
+const option = {
+  year: 'numeric',
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+}
+
+const formattedDate = currentDate
+  .toLocaleDateString('es-ES', option)
+  .toUpperCase()
+date.innerText = formattedDate
